87616bd359d91dcd5ae44ecdde2c2a6639ac690d
selimmartinez <marcosax@dukes.jmu.edu>
2019-08-30 11:37:59 -0400

    java

---
+++ java/hello.java
@@ -0,0 +1,9 @@
+// This is a simple Java program that will display "Hello, World!".
+// Created by Anaseli Marcos-Martinez
+
+class HelloWorld
+{
+    public static void main(String[] args) { 
+        System.out.println("Hello, World");
+    }
+}
