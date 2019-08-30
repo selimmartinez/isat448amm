89e7244709000533614c4e0cbdda3c23f55bca4f
selimmartinez <marcosax@dukes.jmu.edu>
2019-08-29 21:50:39 -0400

    cplusplus

---
+++ cplusplus/hello.cpp
@@ -0,0 +1,13 @@
+//This is a simple program that will display "Hello, World" in C++
+//Created by Anaseli Marcos-Martinez
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, World!";
+    
+    return 0;
+}
