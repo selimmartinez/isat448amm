41762d56575abbfc1f09c4f2089ea23d0beb66b5
selimmartinez <marcosax@dukes.jmu.edu>
2019-09-01 22:35:49 -0400

    javascript/

--- hello.rb
+++
@@ -1,4 +0,0 @@
-#This is a simple program in Ruby that will display "Hello, World!"
-#Created by Anaseli Marcos-Martinez
-
-puts 'Hello, World!'
---
+++ javascript/hello.js
@@ -0,0 +1,5 @@
+// This is a simple program in JavaScript that will display "Hello, World!".
+//Created by Anaseli Marcos-Martinez.
+
+var msg = 'Hello, World!';
+console.log(msg);
