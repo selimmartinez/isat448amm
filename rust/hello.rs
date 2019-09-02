ec355a912193395d7380873f2b8a6f2af57107ed
selimmartinez <marcosax@dukes.jmu.edu>
2019-09-01 22:57:30 -0400

    rust

---
+++ rust/hello.rs
@@ -0,0 +1,7 @@
+// This is a program written in Rust that will display "Hello, World!".
+// Created by Anaseli Marcos-Martinez Ref from URL:https://doc.rust-lang.org/rust-by-example/hello.html
+
+fn main() { //this is the main function
+    println!("Hello, World!"); //print texxt to the console
+
+}
