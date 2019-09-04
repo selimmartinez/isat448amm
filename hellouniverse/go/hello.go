1c6deb9673ae0f6873b441b20bb72954596c97b1
selimmartinez <marcosax@dukes.jmu.edu>
2019-09-04 11:38:16 -0400

    go

---
+++ hellouniverse/go/hello.go
@@ -0,0 +1,10 @@
+// This is a simple program in Go that will display "Hello, World!".
+// Created by Anaseli Marcos-Martinez
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")	
+}
