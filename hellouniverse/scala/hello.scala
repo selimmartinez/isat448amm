352de73cebf80aed39ed49f265488384116384b5
selimmartinez <marcosax@dukes.jmu.edu>
2019-09-05 21:24:38 -0400

    scala

---
+++ hellouniverse/scala/hello.scala
@@ -0,0 +1,9 @@
+// This is a simple program that displays "Hello, World!" 
+// in Scala. Created by Anaseli Marcos-Martinez.
+
+
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        println("Hello, World!")
+    }
+}
