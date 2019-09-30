1b67fcbe998272f92fc731fb9a4ca6196205ff93
selimmartinez <marcosax@dukes.jmu.edu>
2019-09-29 20:18:01 -0400

    vuetify/

---
+++ vuetify/src/views/Dashboard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <h1>Dashboard</h1>
+
+    <v-row>
+      <v-col v-for="sale in sales" :key="`${sale.title}`" cols="12" md="4">
+        <SalesGraph :sale="sale" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        v-for="statistic in statistics"
+        :key="`${statistic.title}`"
+      >
+        <StatisticCard :statistic="statistic" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-snackbar v-model="snackbar">
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
+      <v-btn color="pink" text @click="snackbar = false">
+        Close
+      </v-btn>
+    </v-snackbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  // Excluded for brevity
+}
+</script>
